e2e566fc4dab4cbc7a465624146c4b14018349ce
DadoKoz <dado.kozaragic@gmail.com>
2025-08-28 21:34:02 +0200

    rollback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { usePathname } from "next/navigation";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
-import { useEffect } from "react";
 
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -32,17 +31,6 @@ export default function RootLayout({
   const isEditorPage = pathname === "/editor";
 
   const queryClient = new QueryClient();
-
-  useEffect(() => {
-    // Pokrećemo samo u produkciji (Vercel)
-    if (process.env.NODE_ENV === "production") {
-      const scale = 0.85; // blago udaljavanje (samo 2%)
-      document.body.style.transform = `scale(${scale})`;
-      document.body.style.transformOrigin = "top left";
-      document.body.style.width = `${100 / scale}vw`;
-      document.body.style.height = `${100 / scale}vh`;
-    }
-  }, []);
 
   return (
     <html lang="en">
@@ -72,19 +60,14 @@ export default function RootLayout({
               className={
                 !isAuthPage && !isEditorPage
                   ? "flex flex-col h-screen overflow-auto"
-                  : "h-screen w-screen flex justify-center items-center"
+                  : "h-screen w-screen"
               }
             >
               <div
                 className={
                   !isAuthPage && !isEditorPage
-                    ? "flex-1 max-w-7xl mx-auto w-full px-4 py-4 mt-16"
-                    : "w-full max-w-[98%] max-h-[98%]"
-                }
-                style={
-                  isAuthPage || isEditorPage
-                    ? { transform: "scale(0.98)", transformOrigin: "top left" }
-                    : undefined
+                    ? "flex-1 max-w-7xl mx-auto w-full px-4 py-4 mt-16" // mt-16 rezerviše prostor za StatusBar
+                    : "w-full h-full"
                 }
               >
                 {children}
